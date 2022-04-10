e0a555a04acfc6c6090dd865608bc65485ca2e4c
Aman Shygaibaev <aman.shygaibaev@gmail.com>
2022-04-10 23:15:32 +0300

    bot start command has added

--- program/main.py
+++ program/main.py
@@ -1,3 +1,35 @@
+from email import message
+import telebot
+import psycopg2
 
-import datetime
+bot = telebot.TeleBot("1820483359:AAHjQnauQ3mGC7BIEeGDYinSyTAyNu1Bm6o",parse_mode = None)
+
+
+@bot.message_handler(commands=['start'])
+def reply_to_user(message):
+	bot.send_message(message.chat.id,
+	"Hello!\n" + 
+	"My name is Habit changer\n" +
+	"/set_habit is the command to set a new habit, that you want to follow on daily bases\n" +
+	"/give_up_habbit is the command to give up a bad habit, that you have, and you want to stop do it\n"
+	"/help is the command to see more commands that I have, and see how exactly to use /set_habit and /give_up_habit commands"
+	)
+
+
+
+@bot.message_handler(commands=['view'])
+def view_chat_info(message):
+	print(message.chat.id)
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+
+
+
+
+
+bot.infinity_polling()
+
 
