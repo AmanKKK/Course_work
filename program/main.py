fdfc68a724066f4a10888d1b2998d4a19f62efa9
Aman Shygaibaev <aman.shygaibaev@gmail.com>
2022-04-20 07:30:48 +0300

    set habit command has added

--- program/main.py
+++ program/main.py
@@ -1,13 +1,11 @@
+from asyncio.windows_events import NULL
 from tg_token import tg_token
 import telebot
 import database_connection
 from database_connection import cursor
 from database_connection import conn
 
-#cursor.execute('SELECT years FROM aman ' + 
-#                'WHERE id = 1')
-#for row in cursor:
-#    print(row)
+
 
 bot = telebot.TeleBot(tg_token,parse_mode = None)
 
@@ -23,21 +21,51 @@ def reply_to_user(message):
 	"/give_up_habbit is the command to give up a bad habit, that you have, and you want to stop do it\n"
 	"/help is the command to see more commands that I have, and see how exactly to use /set_habit and /give_up_habit commands"
 	)
-	isNotConnectedToBot = True
-	
 	user_id = str(message.from_user.id)
 	cursor.execute('INSERT INTO tg_users_data(user_name,tg_user_id) ' + 
 	              'VALUES(' + '\'' + message.from_user.first_name + '\'' + ',' + user_id +');')
 	conn.commit()
+#не рабочая функция, от слова совсем
+@bot.message_handler(commands=['set_habit'])
+def set_habit(message):
+	bot.send_message(message.chat.id,
+	"Set new habit\n"
+	)
+	@bot.message_handler(content_types=['text'])
+	def insert_habit(message):
+		user_id = str(message.from_user.id)
+		data_to_insert = {
+			'habit_name': NULL,
+			'tg_habit_type': NULL,
+			'period_time': NULL,
+			'notif_time': NULL
+		}
+		print(message.text + '\n')
+		message_parse = message.text.split()
+		data_to_insert['habit_name'] = message_parse[0]
+		data_to_insert['tg_habit_type'] = message_parse[1]
+		data_to_insert['period_time'] = message_parse[2]
+		data_to_insert['notif_time'] = message_parse[3]
+		cursor.execute('UPDATE tg_users_data SET habit_counter = 1 WHERE tg_user_id ='+ user_id +';')
+		conn.commit()
+		habit_id = message.from_user.id + 1
+		str_habit_id = str(habit_id)
+		cursor.execute(
+		        'INSERT INTO habits_list(tg_user_id,habit_name,tg_habit_type,period_time,notif_time,habit_id)' +
+				' VALUES('  + user_id + 
+		        ',' + '\'' + data_to_insert['habit_name'] + '\'' + 
+				',' + '\'' + data_to_insert['tg_habit_type'] + '\'' + 
+			    ',' + data_to_insert['period_time'] + 
+			    ',' + '\'' + data_to_insert['notif_time'] + '\'' + 
+				',' + str_habit_id +
+			    ');'
+		)
+		conn.commit()
+		
 
 
-@bot.message_handler(commands=['view'])
-def view_chat_info(message):
-	print(message.chat.id)
+		
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
 
 
 
