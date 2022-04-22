9eaf210c3ca61af566c45ffce444d2437e2ca94c
Aman Shygaibaev <aman.shygaibaev@gmail.com>
2022-04-22 16:55:49 +0300

    give_up_habit command has added

--- program/main.py
+++ program/main.py
@@ -1,10 +1,11 @@
 from asyncio.windows_events import NULL
+from pickle import TRUE
 from tg_token import tg_token
 import telebot
 import database_connection
 from database_connection import cursor
 from database_connection import conn
-
+import re
 
 
 bot = telebot.TeleBot(tg_token,parse_mode = None)
@@ -27,12 +28,13 @@ def reply_to_user(message):
 	conn.commit()
 #не рабочая функция, от слова совсем
 @bot.message_handler(commands=['set_habit'])
-def set_habit(message):
+def set_habit_reply(message):
 	bot.send_message(message.chat.id,
 	"Set new habit\n"
 	)
-	@bot.message_handler(content_types=['text'])
+	@bot.message_handler(regexp="POS")
 	def insert_habit(message):
+		print('passed')
 		user_id = str(message.from_user.id)
 		data_to_insert = {
 			'habit_name': NULL,
@@ -46,8 +48,6 @@ def set_habit(message):
 		data_to_insert['tg_habit_type'] = message_parse[1]
 		data_to_insert['period_time'] = message_parse[2]
 		data_to_insert['notif_time'] = message_parse[3]
-		cursor.execute('UPDATE tg_users_data SET habit_counter = 1 WHERE tg_user_id ='+ user_id +';')
-		conn.commit()
 		habit_id = message.from_user.id + 1
 		str_habit_id = str(habit_id)
 		cursor.execute(
@@ -61,7 +61,47 @@ def set_habit(message):
 			    ');'
 		)
 		conn.commit()
+@bot.message_handler(commands=['give_up_habit'])
+def give_up_habit_reply(message):
+	bot.send_message(message.chat.id,
+	"Set a habit,that you want to give up!")
+	@bot.message_handler(regexp="NEG")
+	def give_up_habit(message):
+		user_id = str(message.from_user.id)
+		data_to_insert = {
+			'habit_name': NULL,
+			'tg_habit_type': NULL,
+			'period_time': NULL,
+			'notif_time': NULL
+		}
+		message_parse = message.text.split()
+		data_to_insert['habit_name'] = message_parse[0]
+		data_to_insert['tg_habit_type'] = message_parse[1]
+		data_to_insert['period_time'] = message_parse[2]
+		data_to_insert['notif_time'] = message_parse[3]
+		habit_id = message.from_user.id + 1
+		str_habit_id = str(habit_id)
+		cursor.execute(
+		        'INSERT INTO habits_list(tg_user_id,habit_name,tg_habit_type,period_time,notif_time,habit_id)' +
+				' VALUES('  + user_id + 
+		        ',' + '\'' + data_to_insert['habit_name'] + '\'' + 
+				',' + '\'' + data_to_insert['tg_habit_type'] + '\'' + 
+			    ',' + data_to_insert['period_time'] + 
+			    ',' + '\'' + data_to_insert['notif_time'] + '\'' + 
+				',' + str_habit_id +
+			    ');'
+		)
+		conn.commit()
+
+	
+	
+
+
+
+
 		
+
+
 
 
 		
